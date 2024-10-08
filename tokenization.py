663d57473bc787a746bda4b00c1f596d86dbf612
ChanceMaker <cedrickbaker7@gmail.com>
2024-10-08 01:26:38 -0500

    else

--- tokenization.py
+++ tokenization.py
@@ -8,7 +8,3 @@ from nltk.tokenize import word_tokenize
 text = "Hello, welcome to the world of NLP!"
 tokens = word_tokenize(text)
 print(tokens)
-<<<<<<< HEAD
- 
-=======
->>>>>>> 00d454f29872fcef3720c02f90f69504fd087d58
